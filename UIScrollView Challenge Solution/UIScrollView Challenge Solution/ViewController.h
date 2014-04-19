9a222f674b9513207803c57900cf492ab38414a6
Sven Jansson <sven@Janssons-iMac.local>
2014-04-20 00:56:41 +0200

    Done

--- UIScrollView Challenge Solution/UIScrollView Challenge Solution/ViewController.h
+++ UIScrollView Challenge Solution/UIScrollView Challenge Solution/ViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIScrollViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *globalImageView;
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView;
 
 @end
--- UIScrollView Challenge Solution/UIScrollView Challenge Solution/ViewController.m
+++ UIScrollView Challenge Solution/UIScrollView Challenge Solution/ViewController.m
@@ -18,6 +18,13 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.globalImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"worldMap.jpg"]];
+    self.scrollView.contentSize = self.globalImageView.frame.size;
+    [self.scrollView addSubview:self.globalImageView];
+    self.scrollView.maximumZoomScale = 2.0;
+    self.scrollView.minimumZoomScale = 0.4;
+    self.scrollView.delegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +32,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.globalImageView;
+}
+
 
 @end
